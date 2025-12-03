7fe423b06d706a9ebc3d9b0a007a57f4b77f004b
ad-grupo2-dw <adgrupo2dw@gmail.com>
2025-12-03 14:56:25 -0500

    DROP, CREATE, and USE eholi;

--- database/script.sql
+++ database/script.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `eholi`
 --
+DROP DATABASE IF EXISTS eholi;
+CREATE DATABASE eholi CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE eholi;
 
 -- --------------------------------------------------------
 
